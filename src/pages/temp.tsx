a7b7392b320b437ed46df3dbab10019aae7c9fc1
Kamil Modzelewski <modzelewski.kamil@gmail.com>
2022-08-30 12:11:37 +0200

    Temp test

---
+++ src/pages/temp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TempPage = () => {
+  return (
+    <iframe style={{position: "absolute", width: "80vw", height: "80vh"}}
+            src={"https://miamivirtualmuseum.shapespark.com/movie_theater/"}>
+    </iframe>
+  );
+}
+
+export default TempPage;
